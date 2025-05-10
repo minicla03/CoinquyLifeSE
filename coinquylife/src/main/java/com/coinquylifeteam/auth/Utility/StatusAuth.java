584c38923b27fde78a50e00063728a6c7d126be7
clacla <mininnoclaudio@gmail.com>
2025-05-10 17:26:48 +0200

    Enhance authentication system with registration functionality and UI improvements

---
+++ coinquylife/src/main/java/com/coinquylifeteam/auth/Utility/StatusAuth.java
@@ -0,0 +1,11 @@
+package com.coinquylifeteam.auth.Utility;
+
+public enum StatusAuth
+{
+    SUCCESS,
+    USER_NOT_FOUND,
+    INVALID_CREDENTIALS,
+    USER_ALREADY_EXISTS,
+    TOKEN_EXPIRED,
+    TOKEN_INVALID
+}
