0a4db61b072fb197e5fe7507edc10cc0d512dc44
clacla <mininnoclaudio@gmail.com>
2025-05-10 09:40:57 +0200

    init

---
+++ coinquylife/src/test/java/com/coinquyteam/coinquylife/CoinquylifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coinquyteam.coinquylife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinquylifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
