087245f4e2948f916ea6e2bb4dbed00011247be7
luxiqqq <luciasimeone03@gmail.com>
2025-05-11 17:29:28 +0200

    modifiche mio branch

--- coinquylife/src/main/resources/static/authUI/authLogic.js
+++ coinquylife/src/main/resources/static/authUI/authLogic.js
@@ -14,25 +14,34 @@ document.querySelector("#registerForm button").addEventListener("click", async (
     const data = {
         username: inputs[2].value,
         password: inputs[4].value,
+        name: inputs[0].value,
+        surname: inputs[1].value,
+        email: inputs[3].value,
     };
 
     try {
+        const res = await fetch("http://localhost:8080/rest/auth/register", {
             method: "POST",
             headers: {
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(data),
         });
 
+        const result = await res.text();
         if (res.ok) {
             alert("Registrazione completata!");
             showLogin();
         } else {
+            alert("Errore: " + result);
         }
     } catch (error) {
         alert("Errore di rete: " + error.message);
     }
 });
 
+document.querySelector("#loginForm").addEventListener("submit", async (event) => {
+    event.preventDefault(); // Previene il comportamento predefinito del form
     const inputs = document.querySelectorAll("#loginForm input");
     const data = {
         username: inputs[0].value,
@@ -40,17 +49,27 @@ document.querySelector("#registerForm button").addEventListener("click", async (
     };
 
     try {
+        const res = await fetch(`/rest/auth/login`, {
             method: "POST",
             headers: {
+                "Content-Type": "application/json",
             },
             body: JSON.stringify(data),
         });
 
+        const result = await res.text();
         if (res.ok) {
             alert("Login effettuato!");
+            window.location.href = "/home";
         } else {
+            alert("Errore: " + result);
         }
     } catch (error) {
         alert("Errore di rete: " + error.message);
     }
+});
+
+document.querySelector("#loginForm button").addEventListener("click", async (event) => {
+    event.preventDefault(); // Previene il doppio invio del form
+    document.querySelector("#loginForm").dispatchEvent(new Event("submit"));
 });
