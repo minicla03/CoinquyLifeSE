9c090581a8160928a5c4b64f98829f3df09600e5
clacla <mininnoclaudio@gmail.com>
2025-05-21 19:56:52 +0200

    Implement Expense management features; add Expense and ExpenseService classes, create ExpenseController, update application properties, and modify Dockerfile

--- AuthMicroService/src/main/java/com/coinquyteam/authApplication/AuthMicroServiceApplication.java
+++ AuthMicroService/src/main/java/com/coinquyteam/authApplication/AuthMicroServiceApplication.java
@@ -3,10 +3,12 @@ package com.coinquyteam.authApplication;
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;;
 
 @SpringBootApplication
 @ApplicationPath("/rest")
+@EnableMongoRepositories
 public class AuthMicroServiceApplication extends ResourceConfig
 {
     public AuthMicroServiceApplication()
--- ExpenseMicroService/src/main/java/com/coinquyteam/expenseApplication/ExpenseMicroServiceApplication.java
+++ ExpenseMicroService/src/main/java/com/coinquyteam/expenseApplication/ExpenseMicroServiceApplication.java
@@ -4,9 +4,11 @@ import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @ApplicationPath("/rest")
+@EnableMongoRepositories
 public class ExpenseMicroServiceApplication extends ResourceConfig
 {
     public ExpenseMicroServiceApplication()
